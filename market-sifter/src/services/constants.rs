7fa01e715a8f15408593e29b4596bc6b039fc31f
Oleksii Logvin <oleksiilogvin@gmail.com>
2024-07-18 22:18:21 +0300

    Extend max cap

--- market-sifter/src/services/constants.rs
+++ market-sifter/src/services/constants.rs
@@ -2,5 +2,5 @@ pub const TOP_COINS_TOTAL: u8 = 10;
 pub const TOP_COINS_IN_CATEGORY: u8 = 10;
 pub const TOP_COINS_FOR_FINAL_LIST: usize = 1;
 pub const MIN_MARKET_CAP: u16 = 1;
-pub const MAX_MARKET_CAP: u16 = 500;
+pub const MAX_MARKET_CAP: u16 = 1000;
 pub const CATEGORY_SEPARATOR: &str = ", ";
