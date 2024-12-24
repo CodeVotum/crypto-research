2c15ecb8cdc3a5714e08b647f543f2a35d27002d
Oleksii Logvin <oleksiilogvin@gmail.com>
2024-12-24 12:58:23 +0200

    Tighten filters, update info

--- market-sifter/src/services/constants.rs
+++ market-sifter/src/services/constants.rs
@@ -2,5 +2,5 @@ pub const TOP_COINS_TOTAL: u8 = 10;
 pub const TOP_COINS_IN_CATEGORY: u8 = 10;
 pub const TOP_COINS_FOR_FINAL_LIST: usize = 1;
 pub const MIN_MARKET_CAP: u16 = 100;
-pub const MAX_MARKET_CAP: u16 = 1000;
+pub const MAX_MARKET_CAP: u16 = 300;
 pub const CATEGORY_SEPARATOR: &str = ", ";
